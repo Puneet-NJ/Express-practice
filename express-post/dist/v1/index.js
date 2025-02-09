dc238d46f08b098f0d58d188f81527ca56e6b609
Puneet NJ <lpuneetnj@gmail.com>
2025-02-10 02:02:40 +0530

    delete node modules and dist folder

--- express-post/dist/v1/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.router = void 0;
-const express_1 = require("express");
-const post_1 = require("./routes/post");
-exports.router = (0, express_1.Router)();
-exports.router.use("/post", post_1.postRouter);
